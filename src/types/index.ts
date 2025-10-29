73499e4c2a53cbc707a640148a16cfe7eb8636ef
Hakimuddin Chundawada <hakim.badri53@gmail.com>
2025-10-30 01:25:26 +0530

    updated types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,17 +1,25 @@
-export type Account = { amount: number; isBilled: boolean; };
-export type YearDataMap = Record<number, {
+export type YearKey = 2025 | 2024 | 2023 | 2022;
+
+export type Account = {
+    amount: number;
+    isBilled: boolean;
+};
+
+export type YearDataItem = {
     total_amount: number;
-    price?: number;
-    acc1?: Account;
-    acc2?: Account;
-    acc3?: Account;
-    acc4?: Account;
-}>;
+    acc1: Account;
+    acc2: Account;
+    acc3: Account;
+    acc4: Account;
+};
+
+
 
 export type Entry = {
     service: { _id: string; name: string };
     act: { _id: string; name: string };
     quote: string;
     remarks: string;
-    yearData: YearDataMap;
+    yearData: Record<YearKey, YearDataItem>;
+    _id?: string;
 };
